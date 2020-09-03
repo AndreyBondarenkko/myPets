d25a85cd244d15021fb83a3e978c8e13cce73ae9
easywebAndrey <andreybondarenko1995@gmail.com>
2020-09-03 09:04:12 +0200

    done desctop version

--- app/js/main.js
+++ app/js/main.js
@@ -13,16 +13,52 @@
     };
 
     $(document).ready(function () {
+
         //Выравнивание блоков по высоте
-        heightBlock('.card .card-box__text'); //какой блок выравниваем
-        heightBlock('.product .product-about .box'); //какой блок выравниваем
+        heightBlock('.card .card-box__text');
+        heightBlock('.product .product-about .box');
+        heightBlock('.post .post-desc');
+        heightBlock('.post .post-more li'); 
 
         //block hidden
-        let btn = document.querySelector(".guide-btn");
+        let btn = document.querySelector(".link-btn");
         btn.onclick = () => {
             let hiddenBlock = document.querySelector(".link-guide");
             hiddenBlock.classList.toggle("hidden");
         }
+
+        //accordion
+        function accordionBlock() {
+            var acc = document.getElementsByClassName("faq-accordion");
+            var i;
+
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function () {
+
+                    this.classList.toggle("active");
+
+                    let panel = this.nextElementSibling;
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                    }
+                });
+            }
+
+        }
+        accordionBlock();
+
+        //menu
+        let boxMenu = document.querySelector("#topnav");
+        let btnClick = document.querySelector(".btn-mobileMenu");
+
+        btnClick.addEventListener("click", ()=>{
+            boxMenu.classList.toggle("open");
+            btnClick.classList.toggle("active");
+        })
+
+
 
     });
 
