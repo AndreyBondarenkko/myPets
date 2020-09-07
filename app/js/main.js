99aff9d7c3fa32cadfd82681c54cc2fed362af97
easywebAndrey <andreybondarenko1995@gmail.com>
2020-09-08 01:36:07 +0200

    fix bug with images bg

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,154 @@
-!function(e){function t(t){var o=0;e(t).each(function(){e(this).removeAttr("style"),e(this).height()>o&&(o=e(this).height())}),e(t).height(o)}e(document).ready(function(){t(".card .card-box__text"),t(".product .product-about .box"),t(".post .post-more li");let o=document.querySelector(".link-btn"),l=document.querySelector(".link-guide");o.onclick=(()=>{let e=this.querySelector(".show");o.classList.toggle("active");let t=l.classList.contains("hidden");e.innerHTML=1==!t?"Show":"Hide",l.classList.toggle("hidden")}),function(){let e=document.querySelectorAll(".faq-accordion");for(let t=0;t<e.length;t++){let o=e[t].nextElementSibling;e[t].classList.contains("active")&&(o.style.maxHeight=o.scrollHeight+"px"),e[t].addEventListener("click",()=>{e[t].classList.toggle("active"),o.style.maxHeight?o.style.maxHeight=null:o.style.maxHeight=o.scrollHeight+"px"})}}();let c=document.querySelector("#topnav"),n=document.querySelector(".btn-mobileMenu"),i=document.querySelector("body");if(n.addEventListener("click",()=>{c.classList.toggle("open"),n.classList.toggle("active"),i.classList.toggle("hidden")}),e("#owl-example").owlCarousel({items:4,dots:!0,responsive:{320:{items:1},768:{items:2},992:{items:3}}}),e("#owl-carusel-post").owlCarousel({items:4,arrows:!1,responsive:{320:{items:1},500:{items:2},768:{items:2},992:{items:3},1201:{items:4}}}),document.documentElement.clientWidth<768){!function(){let e=document.querySelectorAll(".product-thumb"),t=document.querySelectorAll(".product-title");for(let o=0;o<e.length;o++)for(let l=0;l<t.length;l++)o==l&&e[o].prepend(t[l])}(),function(){let e=document.querySelector(".desc-image");document.querySelector(".flex-footer-contacts").append(e)}()}if(console.log(document.documentElement.clientWidth),document.documentElement.clientWidth<576){!function(){let e=document.querySelector(".resume"),t=document.querySelector(".blog-doctorAbout");e.append(t)}(),e(".flex-img > img").attr("src", "images/Dogbgm.png")}}),window.onload=function(){}}(jQuery);
+(function ($) {
+
+    //Выравнивание блоков по высоте
+    function heightBlock(class_block) {
+        var max_col_height = 0;
+        $(class_block).each(function () {
+            $(this).removeAttr('style');
+            if ($(this).height() > max_col_height) {
+                max_col_height = $(this).height();
+            }
+        });
+        $(class_block).height(max_col_height);
+    };
+
+    $(document).ready(function () {
+
+        //Выравнивание блоков по высоте
+        heightBlock('.card .card-box__text');
+        heightBlock('.product .product-about .box');
+        heightBlock('.post .post-more li');
+        // heightBlock('.post .post-inner .caption');
+
+        //block hidden
+        let btn = document.querySelector(".link-btn");
+        let hiddenBlock = document.querySelector(".link-guide");
+
+        btn.onclick = () => {
+            let span = this.querySelector(".show");
+            btn.classList.toggle("active");
+            let findC = hiddenBlock.classList.contains("hidden");
+            !findC == true ? span.innerHTML = "Show" : span.innerHTML = "Hide";
+            hiddenBlock.classList.toggle("hidden");
+        }
+
+
+        //accordion
+        function accordionBlock() {
+
+            let acc = document.querySelectorAll(".faq-accordion");
+            for (let k = 0; k < acc.length; k++) {
+                let panel = acc[k].nextElementSibling;
+                let findC = acc[k].classList.contains("active");
+
+                if (findC) {
+                    panel.style.maxHeight = panel.scrollHeight + "px"
+                }
+
+                acc[k].addEventListener("click", () => {
+
+                    acc[k].classList.toggle("active");
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                    }
+                })
+            }
+        }
+        accordionBlock();
+
+        //menu
+        let boxMenu = document.querySelector("#topnav");
+        let btnClick = document.querySelector(".btn-mobileMenu");
+        let body = document.querySelector("body");
+
+        btnClick.addEventListener("click", () => {
+            boxMenu.classList.toggle("open");
+            btnClick.classList.toggle("active");
+            body.classList.toggle("hidden");
+        })
+
+
+        // slider
+        $('#owl-example').owlCarousel({
+            items: 4,
+            dots: true,
+            responsive: {
+                320: {
+                    items: 1,
+                },
+                769: {
+                    items: 2,
+                },
+                992: {
+                    items: 3,
+                }
+            }
+        })
+
+        $('#owl-carusel-post').owlCarousel({
+            items: 4,
+            arrows: false,
+            responsive: {
+                320: {
+                    items: 1,
+                },
+                500: {
+                    items: 2,
+                },
+                768: {
+                    items: 2,
+                },
+                992: {
+                    items: 3,
+                },
+                1201: {
+                    items: 4,
+                }
+            }
+        }) 
+
+
+        if (document.documentElement.clientWidth < 768) {
+            //перенос заголовка
+            function chengeTitle() {
+                let productTitle = document.querySelectorAll(".product-thumb");
+                let title = document.querySelectorAll(".product-title");
+
+                for (let k = 0; k < productTitle.length; k++) {
+                    for (let i = 0; i < title.length; i++) {
+                        k == i ? productTitle[k].prepend(title[i]) : false;
+                    }
+                }
+            }
+            chengeTitle();
+            //перенос картинки футер
+            function footerImage() {
+                let protect = document.querySelector(".protect");
+                let phone = document.querySelector(".phone");
+                let fotImage = document.querySelector(".desc-image");
+                let footerBlock = document.querySelector(".flex-footer-contacts");
+                footerBlock.append(fotImage);
+                phone.after(protect);
+            }
+            footerImage();
+        }
+
+        if (document.documentElement.clientWidth < 576) {
+            function resumeBlock() {
+                let resume = document.querySelector(".resume")
+                let resumeContent = document.querySelector(".blog-doctorAbout");
+                resume.append(resumeContent);
+            }
+            resumeBlock()
+            
+            
+            //change src
+            $('.flex-img > img').attr('src', '../images/Dogbgm.png');
+        }
+    });
+
+    window.onload = function () {};
+
+})(jQuery);
